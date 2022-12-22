41543dff4a631ff4303ff44f86d7ee0cd2ffd81a
AJAskey <ajaskey34@gmail.com>
2022-12-22 15:51:56 -0500

    Update

--- src/net/ajaskey/market/tools/fred/executables/FredUpdateLocal.java
+++ src/net/ajaskey/market/tools/fred/executables/FredUpdateLocal.java
@@ -20,8 +20,18 @@ package net.ajaskey.market.tools.fred.executables;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import net.ajaskey.common.DateTime;
 import net.ajaskey.common.Debug;
 import net.ajaskey.common.Utils;
 import net.ajaskey.market.tools.fred.ApiKey;
@@ -30,13 +40,19 @@ import net.ajaskey.market.tools.fred.LocalFormat;
 import net.ajaskey.market.tools.fred.queries.Observations;
 
 /**
- * Class used to Initialize a new local FRED library.
+ * Class used to maintain data in a local FRED library.
  */
 public class FredUpdateLocal {
 
   public enum FredMode {
     FULL, UPDATE;
   }
+
+  private static FredMode mode = FredMode.UPDATE;
+  // private static String fredlib = "D:/data2/MA/CSV Data/FRED-Download";
+  private static String fredlib  = "fredlib";
+  private static String inputIds = "input/filteredSeriesSummary.txt";
+  private static int    downloadKnt;
 
   /**
    * Main procedure:
@@ -58,53 +74,84 @@ public class FredUpdateLocal {
    * </p>
    *
    *
-   * @param args None used.
-   * @throws IOException
-   * @throws FileNotFoundException
+   * @param args Command line arguements. See <b>setFromCli()</b>.
    */
-  public static void main(String[] args) throws FileNotFoundException, IOException {
+  public static void main(String[] args) {
 
     /**
-     * Key internal variables:
+     * Key command line inputs:
      *
-     * mode - FULL or UPDATE
+     * mode - FULL or UPDATE (Default : UPDATE)
      *
-     * fredlib - Directory with current and for new data files.
+     * fredlib - Directory with current and for new data files. (Default :
+     * "fredlib")
      *
-     * inputIds - File with LocalFormat data listing Ids to be processed.
+     * inputIds - File with LocalFormat data listing Series IDs to be processed.
+     * (Default : "out/filteredSeriesSummary.txt")
      *
      */
-    final FredMode mode = FredMode.UPDATE;
-    final String fredlib = "D:\\data2\\MA\\CSV Data\\FRED-Download";
-    final String inputIds = "out/filteredSeriesSummary.txt";
 
     Utils.makeDir("debug");
     Utils.makeDir("out");
+    Utils.makeDir(FredUpdateLocal.fredlib);
+
+    downloadKnt = 0;
+
+    FredUpdateLocal.setFromCli(args);
 
     Debug.init("debug/FredUpdateLocal.dbg", java.util.logging.Level.INFO);
 
     ApiKey.set();
 
-    Debug.LOGGER.info(String.format("Mode=%s    fredlib=%s", mode, fredlib));
+    String dbg = String.format("Mode=%s    fredlib=%s    inputIds=%s", FredUpdateLocal.mode, FredUpdateLocal.fredlib, FredUpdateLocal.inputIds);
+    Debug.LOGGER.info(dbg);
 
-    final List<LocalFormat> lfList = LocalFormat.readSeriesList(inputIds, "FredLib");
+    final List<LocalFormat> lfList = LocalFormat.readSeriesList(FredUpdateLocal.inputIds, "FredLib");
+    final List<LocalFormat> overridesList = LocalFormat.readSeriesList("input/overrides.txt", "FredLib");
 
     String ids = "Processing Ids :" + Utils.NL;
+    for (final LocalFormat lf : overridesList) {
+      ids += lf.getId() + "    (Override)" + Utils.NL;
+    }
     for (final LocalFormat lf : lfList) {
       ids += lf.getId() + Utils.NL;
     }
     Debug.LOGGER.info(ids);
 
+    for (final LocalFormat lf : overridesList) {
+      FredUpdateLocal.process(lf, FredUpdateLocal.fredlib, 5, 8);
+    }
+
+    dbg = "";
+
     for (final LocalFormat lf : lfList) {
-      if (mode == FredMode.FULL) {
-        FredUpdateLocal.process(lf, fredlib, 5, 8);
+      boolean success = false;
+      if (FredUpdateLocal.mode == FredMode.FULL) {
+        success = FredUpdateLocal.process(lf, FredUpdateLocal.fredlib, 5, 8);
       }
       else {
-        FredUpdateLocal.update(lf, fredlib, 5, 8);
+        success = FredUpdateLocal.update(lf, FredUpdateLocal.fredlib, 5, 8);
+      }
+      if (success) {
+        lf.setLocalFileDate(new DateTime());
+        dbg += lf.getId() + Utils.NL;
+        System.out.println(lf.getId());
       }
     }
 
-    Debug.LOGGER.info(String.format("%n%n------%nProcessing completed"));
+    Debug.LOGGER.info(Utils.NL + dbg);
+
+    Collections.sort(lfList, LocalFormat.sorter);
+    try (PrintWriter pw = new PrintWriter("out/Fred-Status.txt")) {
+      for (LocalFormat lf : lfList) {
+        pw.println(lf.formatlineRel());
+      }
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    Debug.LOGGER.info(String.format("%n%n------%nProcessing completed. Downloaded = %d", downloadKnt));
   }
 
   /**
@@ -116,7 +163,9 @@ public class FredUpdateLocal {
    * @throws FileNotFoundException
    * @throws IOException
    */
-  static void process(LocalFormat lf, String fredlib, int retries, int delay) {
+  static boolean process(LocalFormat lf, String fredlib, int retries, int delay) {
+
+    boolean success = false;
 
     Debug.LOGGER.info(String.format("Processing... %n%s", lf));
 
@@ -128,7 +177,9 @@ public class FredUpdateLocal {
 
         Debug.LOGGER.info(String.format("Returned observations for %s%n%s", lf.getId(), obs));
 
-        FredUtils.writeToLibNew(obs, lf, fredlib);
+        FredUtils.writeToLib(obs, lf, fredlib);
+        success = true;
+        downloadKnt++;
       }
       else {
         Debug.LOGGER.info(String.format("Observations for %s is not valid.", lf.getId()));
@@ -137,6 +188,8 @@ public class FredUpdateLocal {
     else {
       Debug.LOGGER.info(String.format("Observations for %s is null.", lf.getId()));
     }
+
+    return success;
   }
 
   /**
@@ -144,29 +197,106 @@ public class FredUpdateLocal {
    * @param lf
    * @param fredlib
    */
-  private static void update(LocalFormat lf, String fredlib, int retries, int delay) {
+  static boolean update(LocalFormat lf, String fredlib, int retries, int delay) {
+
+    boolean success = false;
 
     Debug.LOGGER.info(String.format("%n---%nChecking %s", lf.getId()));
     boolean doProcess = false;
 
-    if (lf.getFrequency().toLowerCase().contains("daily")) {
-      doProcess = true;
-      Debug.LOGGER.info(String.format("doProcess - daily"));
-    }
-    else if (lf.getLocalFileDate() == null) {
+    if (lf.getLocalFileDate() == null) {
       doProcess = true;
       Debug.LOGGER.info(String.format("doProcess - no local file"));
     }
+    else if (lf.getFrequency().toLowerCase().contains("daily")) {
+      doProcess = true;
+      Debug.LOGGER.info(String.format("doProcess - daily"));
+    }
     else {
-      if (lf.getLocalFileDate().isLessThan(lf.getLastUpdate())) {
-        doProcess = true;
-        Debug.LOGGER.info(String.format("doProcess - local file out of date"));
-      }
+      doProcess = updateSeries(lf);
     }
 
     if (doProcess) {
-      FredUpdateLocal.process(lf, fredlib, retries, delay);
+      success = FredUpdateLocal.process(lf, fredlib, retries, delay);
     }
+
+    return success;
+  }
+
+  private static boolean updateSeries(LocalFormat lf) {
+    boolean doProcess = false;
+    if (lf.getLocalFileDate().isLessThan(lf.getLastUpdate())) {
+      doProcess = true;
+      Debug.LOGGER.info(String.format("doProcess - local file out of date"));
+    }
+    return doProcess;
+  }
+
+  /**
+   *
+   * @param args
+   */
+  private static void setFromCli(String[] args) {
+    /**
+     * CLI
+     */
+    final Options options = new Options();
+    final Option debugfile = Option.builder().longOpt("debug").argName("d").hasArg().desc("debug file override").build();
+    final Option fredlib = Option.builder().longOpt("fredlib").argName("l").hasArg().desc("FRED Library Directory").build();
+    final Option mode = Option.builder().longOpt("mode").argName("m").hasArg().desc("Mode of Operation").build();
+    final Option input = Option.builder().longOpt("input").argName("i").hasArg().desc("File of Ids").build();
+    options.addOption(debugfile);
+    options.addOption(fredlib);
+    options.addOption(mode);
+    options.addOption(input);
+
+    final CommandLineParser parser = new DefaultParser();
+    try {
+      final CommandLine line = parser.parse(options, args);
+      if (line.hasOption("debug")) {
+        System.out.println(line.getOptionValue(debugfile));
+      }
+      if (line.hasOption("fredlib")) {
+        System.out.println(line.getOptionValue(fredlib));
+        FredUpdateLocal.setLib(line.getOptionValue(fredlib));
+      }
+      if (line.hasOption("mode")) {
+        System.out.println(line.getOptionValue(mode));
+        FredUpdateLocal.setMode(line.getOptionValue(mode));
+      }
+      if (line.hasOption("input")) {
+        System.out.println(line.getOptionValue(input));
+        FredUpdateLocal.setInput(line.getOptionValue(input));
+      }
+    }
+    catch (final ParseException e1) {
+      e1.printStackTrace();
+    }
+
+  }
+
+  private static void setInput(String infile) {
+    FredUpdateLocal.inputIds = infile;
+
+  }
+
+  private static void setLib(String libdir) {
+    FredUpdateLocal.fredlib = libdir;
+  }
+
+  private static void setMode(String m) {
+
+    try {
+      FredUpdateLocal.mode = FredMode.UPDATE;
+      if (m.toUpperCase().equals("FULL")) {
+        FredUpdateLocal.mode = FredMode.FULL;
+      }
+    }
+    catch (final Exception e) {
+      e.printStackTrace();
+      FredUpdateLocal.mode = FredMode.UPDATE;
+    }
+
   }
 
 }
